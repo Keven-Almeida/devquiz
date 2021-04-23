e1c481462a15100bc8c37d669e4b1eaf44d096fd
keven-almeida <keven.almeida.8179@gmail.com>
2021-04-22 23:06:24 -0300

    primeiro commit

---
+++ lib/challenge/challenge_page.dart
@@ -0,0 +1,41 @@
+import 'package:DevQuiz/challenge/widgets/next_button/next_button_widget.dart';
+import 'package:DevQuiz/challenge/widgets/question_indicator/question_indicator_widget.dart';
+import 'package:DevQuiz/challenge/widgets/quiz/quiz_widget.dart';
+import 'package:flutter/material.dart';
+
+class ChallengePage extends StatefulWidget {
+  @override
+  _ChallengePageState createState() => _ChallengePageState();
+}
+
+class _ChallengePageState extends State<ChallengePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: PreferredSize(
+        child: SafeArea(
+          top: true,
+          child: QuestionIndicatorWidget(),
+        ),
+        preferredSize: Size.fromHeight(60),
+      ),
+      body: QuizWidget(
+        title: "O que o Flutter faz em sua totalidade? ",
+      ),
+      bottomNavigationBar: SafeArea(
+        bottom: true,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              Expanded(child: NextButtonWidget.white(label: "Fácil")),
+              SizedBox(width: 7),
+              Expanded(child: NextButtonWidget.green(label: "Confirmar"))
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
