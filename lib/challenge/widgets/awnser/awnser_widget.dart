6b356a2eab36c07f4f9d7b50a3780fafdeacf59e
keven-almeida <keven.almeida.8179@gmail.com>
2021-04-24 10:05:27 -0300

    adicionados controle de estados e condiconamentos

--- lib/challenge/widgets/awnser/awnser_widget.dart
+++ lib/challenge/widgets/awnser/awnser_widget.dart
@@ -7,11 +7,13 @@ class AwnserWidget extends StatelessWidget {
   final AwnserModel awnser;
   final bool isSelected;
   final VoidCallback onTap;
+  final bool disabled;
 
   const AwnserWidget({
     Key? key,
     required this.awnser,
     required this.onTap,
+    required this.disabled,
     this.isSelected = false,
   }) : super(key: key);
 
@@ -35,48 +37,54 @@ class AwnserWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 4),
-      child: GestureDetector(
-        onTap: onTap,
-        child: Container(
-          padding: EdgeInsets.all(16),
-          decoration: BoxDecoration(
-            color: isSelected ? _selectedColorCardRight : AppColors.white,
-            borderRadius: BorderRadius.circular(10),
-            border: Border.fromBorderSide(
-              BorderSide(
-                color: isSelected ? _selectedBorderCardRight : AppColors.border,
-              ),
-            ),
-          ),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Expanded(
-                child: Text(
-                  awnser.title,
-                  style:
-                      isSelected ? _selectedTextStyleRight : AppTextStyles.body,
+      child: IgnorePointer(
+        ignoring: disabled,
+        child: GestureDetector(
+          onTap: onTap,
+          child: Container(
+            padding: EdgeInsets.all(16),
+            decoration: BoxDecoration(
+              color: isSelected ? _selectedColorCardRight : AppColors.white,
+              borderRadius: BorderRadius.circular(10),
+              border: Border.fromBorderSide(
+                BorderSide(
+                  color:
+                      isSelected ? _selectedBorderCardRight : AppColors.border,
                 ),
               ),
-              Container(
-                  width: 24,
-                  height: 24,
-                  decoration: BoxDecoration(
-                      color: isSelected ? _selectedColorRight : AppColors.white,
-                      borderRadius: BorderRadius.circular(500),
-                      border: Border.fromBorderSide(BorderSide(
-                        color: isSelected
-                            ? _selectedBorderRight
-                            : AppColors.border,
-                      ))),
-                  child: isSelected
-                      ? Icon(
-                          _selectedIconRight,
-                          color: AppColors.white,
-                          size: 16,
-                        )
-                      : null)
-            ],
+            ),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Expanded(
+                  child: Text(
+                    awnser.title,
+                    style: isSelected
+                        ? _selectedTextStyleRight
+                        : AppTextStyles.body,
+                  ),
+                ),
+                Container(
+                    width: 24,
+                    height: 24,
+                    decoration: BoxDecoration(
+                        color:
+                            isSelected ? _selectedColorRight : AppColors.white,
+                        borderRadius: BorderRadius.circular(500),
+                        border: Border.fromBorderSide(BorderSide(
+                          color: isSelected
+                              ? _selectedBorderRight
+                              : AppColors.border,
+                        ))),
+                    child: isSelected
+                        ? Icon(
+                            _selectedIconRight,
+                            color: AppColors.white,
+                            size: 16,
+                          )
+                        : null)
+              ],
+            ),
           ),
         ),
       ),
