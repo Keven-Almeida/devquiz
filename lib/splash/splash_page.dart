10886d024a2012fd6ae48109349b8dbe328a8530
keven-almeida <keven.almeida.8179@gmail.com>
2021-04-24 12:47:17 -0300

    adicionado animação

--- lib/splash/splash_page.dart
+++ lib/splash/splash_page.dart
@@ -1,9 +1,15 @@
 import 'package:DevQuiz/core/core.dart';
+import 'package:DevQuiz/home/home_page.dart';
 import 'package:flutter/material.dart';
 
-class SpashPage extends StatelessWidget {
+class SplashPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    Future.delayed(Duration(seconds: 2)).then((_) => Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(
+          builder: (context) => HomePage(),
+        )));
     return Scaffold(
       body: Container(
         decoration: BoxDecoration(
